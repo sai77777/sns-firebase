6c605d89182c4d89f38c5bef625ff176574359ea
m.sato <m.sato@emusatonoMacBook-Air.local>
2021-04-18 15:33:45 +0900

    検索機能-Algoliaの導入

--- functions/src/algolia.ts
+++ functions/src/algolia.ts
@@ -1,8 +1,8 @@
 import { config } from 'firebase-functions'
 import algoliasearch from 'algoliasearch'
 
-const ALGOLIA_ID = "myfirebase-baa9e"
-const ALGOLIA_ADMIN_KEY = "AIzaSyASvt2hmeOO7ZcLJ0WftyYjSH_N4lSZw34"
+const ALGOLIA_ID = config().algolia.application_id
+const ALGOLIA_ADMIN_KEY = config().algolia.api_key
 const tweetsIndexName = 'tweets'
 
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY)
